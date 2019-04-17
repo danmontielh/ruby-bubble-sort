30bfa8b7f4fc2939a547b74899704b0df2fd46ba
Dan <danmontielh@gmail.com>
2019-04-17 16:43:42 -0500

    separated each  method by file

--- bubble_sorted_by.rb
+++ bubble_sorted_by.rb
@@ -7,21 +7,25 @@ def swipe(arr, index1, index2)
     return arr
 end
 
-#bubble sort 
-
-def bubble_sort(arr)
+def bubble_sort_by(arr)
     sizeArr = arr.length
     until sizeArr <= 1
         tempSizeArray = 0;
         for i in 1..sizeArr-1 do
             if arr[i-1] > arr[i]
-                swipe(arr, i-1, i)
+                if yield(arr[i-1], arr[i]) > 0
+                    swipe(arr, i-1,i )
+                end
                 tempSizeArray = i
             end #if
         end  #for
     sizeArr = tempSizeArray
     end #until
 arr
-end #method 
+end
 
- p bubble_sort([4,3,78,2,0,2])
+sorted_by = bubble_sort_by(["hi", "hey", "hello"]) {|first, second| 
+        first.length - second.length
+} 
+
+p sorted_by
